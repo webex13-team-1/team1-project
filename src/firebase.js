b4e493ebb4862cfd8ade2706858fe68e11ae80bb
mikachi20 <mKs1m400h@gmail.com>
2022-03-07 23:46:50 +0900

    upgrade-vue

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzOeGISdXVx8E6bOnxPM_2H9QnmyxMaP4",
+  authDomain: "webex-team1-project.firebaseapp.com",
+  projectId: "webex-team1-project",
+  storageBucket: "webex-team1-project.appspot.com",
+  messagingSenderId: "136442920603",
+  appId: "1:136442920603:web:55c873c1a2d3fa183d903e",
+  measurementId: "G-C7D6E0T0B3",
+}
+
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
