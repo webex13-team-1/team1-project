b036bd2092edbf8afb9637a949777be62575c828
Koichi Uchida <valleypark_sg02_05@icloud.com>
2022-03-11 18:29:14 +0900

    Make a basic html

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div class="lists">
+      <div class="list">
+        <div class="list-header">
+          <a>Header</a>
+        </div>
+        <div class="card"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      companies: [
+        {
+          companyId: 1,
+          companyName: "Apple",
+          contents: {
+            event: [
+              {
+                title: "〇〇会社夏選考",
+                category: "選考",
+                contents: [],
+              },
+              {
+                title: "〇〇会社秋選考",
+                category: "選考",
+                contents: [],
+              },
+            ],
+          },
+        },
+      ],
+    }
+  },
+}
+</script>
