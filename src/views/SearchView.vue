c79b693451287b90beed7eed12d4bdcd3ee8ca0f
Koichi Uchida <valleypark_sg02_05@icloud.com>
2022-03-12 09:32:16 +0900

    Set data

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -4,4 +4,71 @@
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      companies: [
+        {
+          companyId: 1,
+          companyName: "Apple",
+          companyContents: {
+            event: [
+              {
+                eventId: 1,
+                eventTitle: "Apple会社夏選考",
+                eventCategory: "選考",
+                eventContents: [
+                  {
+                    eventContentCategory: "申し込み",
+                    eventContentMemo: [
+                      {
+                        topic: "ES",
+                        wordCount: 400,
+                        question: "志望動機はなんですか？",
+                        answer: "御社の〇〇〜〜",
+                      },
+                      {
+                        topic: "ES",
+                        wordCount: 500,
+                        question: "ガクチカはなんですか？",
+                        answer: "私のガクチカは〇〇〜〜",
+                      },
+                    ],
+                  },
+                ],
+              },
+              {
+                eventId: 2,
+                eventTitle: "Apple会社秋選考",
+                eventCategory: "選考",
+                eventContents: [],
+              },
+            ],
+          },
+        },
+        {
+          companyId: 2,
+          companyName: "Microsoft",
+          companyContents: {
+            event: [
+              {
+                eventId: 1,
+                eventTitle: "Microsoft会社夏選考",
+                eventCategory: "選考",
+                eventContents: [],
+              },
+              {
+                eventId: 2,
+                eventTitle: "Microsoft会社秋選考",
+                eventCategory: "選考",
+                eventContents: [],
+              },
+            ],
+          },
+        },
+      ],
+    }
+  },
+}
+</script>
