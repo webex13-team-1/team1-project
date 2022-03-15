29c03528ae59487cb7384b5320932e8dbad95f1f
Koichi Uchida <valleypark_sg02_05@icloud.com>
2022-03-15 17:09:27 +0900

    Make a search program

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,6 +1,49 @@
 <template>
   <div class="search">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <p>ジャンル検索</p>
+    <input type="text" list="samplelist" v-model="inputValue" />
+    <!-- <option value="1">選考</option> -->
+  </div>
+  <div class="lists">
+    <div
+      class="list"
+      v-for="searchCompanies in searchGenre"
+      :key="searchCompanies.companyId"
+    >
+      <div class="list-header">
+        <a>Header</a>
+      </div>
+      <div class="cards">
+        <!-- 以下一枚一枚のカード作り -->
+        <div class="card">
+          <!-- :key="content.id"がないとエラーが出る -->
+          <!-- 会社名 -->
+          <div class="company-name">
+            会社名
+            {{ searchCompanies.companyName }}
+          </div>
+          <div
+            class="envents"
+            v-for="event in searchCompanies.companyContents.event"
+            :key="event.eventId"
+          >
+            <div class="event">
+              <a class="event-title">
+                <!-- イベント名 -->
+                {{ event.eventTitle }}
+              </a>
+              <a>/</a>
+              <a class="event-category">
+                <!-- イベント種類 -->
+                {{ event.eventCategory }}
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- 以上一枚一枚のカード作り -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,6 +51,7 @@
 export default {
   data() {
     return {
+      inputValue: "",
       companies: [
         {
           companyId: 1,
@@ -69,6 +113,41 @@ export default {
         },
       ],
     }
+  },
+  //this.companies[0].companyContents.event[0]
+  computed: {
+    searchGenre: function () {
+      const searchWord = this.inputValue
+
+      var searchCompanies = []
+      console.log(searchCompanies)
+
+      for (let i = 0; i < this.companies.length; i++) {
+        var pushCompanyInfomation = {}
+        pushCompanyInfomation.companyId = this.companies[i].companyId
+        pushCompanyInfomation.companyName = this.companies[i].companyName
+
+        var event = []
+        for (
+          let j = 0;
+          j < this.companies[i].companyContents.event.length;
+          j++
+        ) {
+          if (
+            searchWord ==
+            this.companies[i].companyContents.event[j].eventCategory
+          ) {
+            event.push(this.companies[i].companyContents.event[j])
+          }
+        }
+        console.log("--------")
+        if (event.length != 0) {
+          pushCompanyInfomation.companyContents = { event: event }
+          searchCompanies.push(pushCompanyInfomation)
+        }
+      }
+      return searchCompanies
+    },
   },
 }
 </script>
