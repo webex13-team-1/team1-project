39f94b51766db24976aab3c0bad436d7e4b4ef71
Koichi Uchida <valleypark_sg02_05@icloud.com>
2022-03-17 04:49:16 +0900

    Make a sign up basic sysytem

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -6,28 +6,61 @@
       <input type="text" v-model="username" />
     </div>
     <div>
+      <a>email</a>
+      <input type="text" v-model="email" />
+    </div>
+    <div>
       <a>password</a>
       <input type="text" v-model="password" />
     </div>
     <button v-on:click="signUp">SIGNUP</button>
     {{ username }}
+    {{ email }}
     {{ password }}
   </div>
 </template>
 
 <script>
-import { auth, createUser } from "../firebase.js"
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth"
+import { doc, setDoc } from "firebase/firestore"
+import { db } from "../firebase"
+
+const auth = getAuth()
 
 export default {
   data() {
     return {
       username: "",
+      email: "",
       password: "",
     }
   },
   methods: {
-    signUp() {
-      createUser(auth, this.username, this.password)
+    async signUp() {
+      //FireBaseのAuthenticationに登録
+      await createUserWithEmailAndPassword(auth, this.email, this.password)
+
+      //FireBaseのAuthenticationの登録時にAuthenticationに割り当てられたUIDを取得
+      const user = auth.currentUser
+      const uid = user.uid
+
+      //FireBaseのDBに保存する内容のまとめ
+      const signUpInfo = {
+        username: this.username,
+        email: this.email,
+        isGeekSalon: false,
+      }
+
+      //FireBaseのDBに保存
+      const cityRef = doc(db, "users", uid)
+      setDoc(cityRef, signUpInfo)
+    },
+    login() {
+      signInWithEmailAndPassword(auth, this.username, this.password)
     },
   },
 }
