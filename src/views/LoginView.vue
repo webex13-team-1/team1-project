f358644e5c8b5cb6a34217017b217257b6ed3fae
Koichi Uchida <valleypark_sg02_05@icloud.com>
2022-03-17 20:16:41 +0900

    Make login and logout forms

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="login">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="sign-up">
+    <p>新規登録</p>
     <div>
       <a>username</a>
       <input type="text" v-model="username" />
@@ -14,20 +14,42 @@
       <input type="text" v-model="password" />
     </div>
     <button v-on:click="signUp">SIGNUP</button>
-    {{ username }}
-    {{ email }}
-    {{ password }}
+  </div>
+  <div class="login">
+    <p>ログイン</p>
+    <div>
+      <a>email</a>
+      <input type="text" v-model="email" />
+    </div>
+    <div>
+      <a>password</a>
+      <input type="text" v-model="password" />
+    </div>
+    <button v-on:click="signUp">LOGIN</button>
+  </div>
+  <div class="logout">
+    <p>ログアウト</p>
+    <button v-on:click="signUp">LOGOUT</button>
+  </div>
+  <div>
+    <button v-on:click="identifyLoginStatus">STATUS</button>
+    {{ loginStatus }}
+    {{ loginUid }}
   </div>
 </template>
 
 <script>
+//SIGNUP関連
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth"
-import { doc, setDoc } from "firebase/firestore"
+import { doc, setDoc, Timestamp } from "firebase/firestore"
 import { db } from "../firebase"
+
+//ログインSTATUS関連
+import { onAuthStateChanged } from "firebase/auth"
 
 const auth = getAuth()
 
@@ -37,6 +59,10 @@ export default {
       username: "",
       email: "",
       password: "",
+      loginStatus: "",
+      loginUid: "",
+      loginUsername: "",
+      loginEmail: "",
     }
   },
   methods: {
@@ -47,21 +73,60 @@ export default {
       //FireBaseのAuthenticationの登録時にAuthenticationに割り当てられたUIDを取得
       const user = auth.currentUser
       const uid = user.uid
+      const creationDate = user.metadata.creationTime
 
       //FireBaseのDBに保存する内容のまとめ
       const signUpInfo = {
         username: this.username,
         email: this.email,
         isGeekSalon: false,
+        createDate: Timestamp.fromDate(new Date(creationDate)),
+        loginDate: [Timestamp.fromDate(new Date(creationDate))],
+        logoutDate: [],
       }
 
-      //FireBaseのDBに保存
+      console.log(signUpInfo)
+
+      //FireBaseのDBに保存(ドキュメント名=UID)
       const cityRef = doc(db, "users", uid)
       setDoc(cityRef, signUpInfo)
     },
     login() {
       signInWithEmailAndPassword(auth, this.username, this.password)
     },
+    identifyLoginStatus() {
+      onAuthStateChanged(auth, (user) => {
+        console.log("------")
+        if (user) {
+          // const uid = user.uid
+          console.log("login")
+          this.loginstatus = "Logined"
+          // ...
+        } else {
+          console.log("logout")
+          this.loginstatus = "Logout"
+        }
+      })
+      return this.loginstatus
+    },
+  },
+  created() {
+    onAuthStateChanged(auth, (user) => {
+      console.log("------")
+      if (user) {
+        // const uid = user.uid
+        console.log("login")
+        this.loginStatus = "Logined"
+        this.loginUid = user.uid
+        // ...
+      } else {
+        console.log("logout")
+        this.loginstatus = "Logout"
+      }
+    })
+    //ここの関数はmethodsの関数と同じなので、まとめれば！
   },
 }
+
+//関数
 </script>
