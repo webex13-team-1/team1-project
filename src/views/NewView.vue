f2ea1891dc462204f2f24be7ce3c5878e42012f3
Koichi Uchida <valleypark_sg02_05@icloud.com>
2022-03-18 18:21:58 +0900

    Edit db and forms

--- src/views/NewView.vue
+++ src/views/NewView.vue
@@ -1,64 +1,75 @@
 <template>
   <div class="app">
-    <textarea v-model="title" placeholder="タイトルを記入する場所" />
+    <!-- <textarea v-model="title" placeholder="タイトルを記入する場所" />
     <br />
     <textarea
       class="form__textarea"
       v-model="text"
       placeholder="ガクチカを記入する場所"
     />
+    {{ this.title }}
+    {{ this.text }} -->
     <br />
+    <textarea v-model="companyName" placeholder="companyName"></textarea>
+    <textarea v-model="eventTitle" placeholder="eventTitle"></textarea>
+    <textarea v-model="eventCategory" placeholder="eventCategory"></textarea>
+    <br />
+    {{ companyName }}
+    {{ eventTitle }}
+    {{ eventCategory }}
+
     <button v-on:click="postTweet">投稿</button>
-    <!-- 変更点１ -->
+    <!-- 変更点１
     <div>
-      <p v-for="tweet in tweets" :key="tweet.id">
-        {{ tweet.title }}
-        {{ tweet.text }}
+      <p v-for="company in companies" :key="company.id">
+        {{ company.title }}
+        {{ company.text }}
       </p>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
-import { collection, addDoc, getDocs } from "firebase/firestore"
+import { collection, addDoc } from "firebase/firestore"
 import { db } from "../firebase"
 
 export default {
   /* 変更点２ */
   data() {
     return {
-      tweets: [
-        // {
-        //   id: "0GwoGZuhTNhqHQDBeiVW",
-        //   text: "こんにちは、ツイートの本文です。"
-        // }
-      ],
+      companyName: "",
+      eventTitle: "",
+      eventCategory: "",
     }
   },
   methods: {
     postTweet() {
-      /* 変更点１ */
-      const tweet = {
-        text: this.text,
-        title: this.title,
+      const data = {
+        companyId: 1,
+        companyName: this.companyName,
+        companyContents: {
+          event: [
+            {
+              eventId: 1,
+              eventTitle: this.eventTitle,
+              eventCategory: this.eventCategory,
+            },
+          ],
+        },
       }
-      addDoc(collection(db, "tweets"), tweet).then((ref) => {
-        this.tweets.push({
-          id: ref.id,
-          ...tweet,
-        })
-      })
+      console.log(data)
+      addDoc(collection(db, "companies"), data)
     },
   },
-  created() {
-    getDocs(collection(db, "tweets")).then((snapshot) => {
-      snapshot.forEach((doc) => {
-        this.tweets.push({
-          id: doc.id,
-          ...doc.data(),
-        })
-      })
-    })
-  },
+  // created() {
+  //   getDocs(collection(db, "companies")).then((snapshot) => {
+  //     snapshot.forEach((doc) => {
+  //       this.companies.push({
+  //         id: doc.id,
+  //         ...doc.data(),
+  //       })
+  //     })
+  //   })
+  // },
 }
 </script>
